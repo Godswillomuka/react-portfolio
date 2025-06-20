e82eb25934e8d6f92e40131055a35fe79cdbc2a4
Godswill omuka <omukagodswil@gmail.com>
2025-06-20 14:40:08 +0300

    feat: add About section with external CSS styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
       <Footer />
     </div>
   );
---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+    return (
+        <section id="about" className="about-section">
+            <h2>About Me</h2>
+            <p>
+                I am a web developer with a passion for creating dynamic and responsive web applications. 
+                With a background in computer science, I have honed my skills in HTML, CSS, JavaScript, and various frameworks.
+            </p>
+        </section>
+    );
+}
+
+export default About;
