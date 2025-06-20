5d41740de99c02df6145667a15355225efa1bbcd
Godswill omuka <omukagodswil@gmail.com>
2025-06-20 14:45:11 +0300

    feat: add Projects section with responsive cards and styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
+import Projects from './pages/Projects';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Projects />
       <Footer />
     </div>
   );
