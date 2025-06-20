8232deafbc29bb7acde5d4202e6c278f6fc99355
Godswill omuka <omukagodswil@gmail.com>
2025-06-20 15:40:47 +0300

    solved the link on router issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,10 +17,10 @@ function Navbar() {
       </div>
 
       <ul className={`navbar-links ${isOpen ? "active" : ""}`}>
-        <li><a href="#home" onClick={() => setIsOpen(false)}>Home</a></li>
-        <li><a href="#about" onClick={() => setIsOpen(false)}>About</a></li>
-        <li><a href="#projects" onClick={() => setIsOpen(false)}>Projects</a></li>
-        <li><a href="#contact" onClick={() => setIsOpen(false)}>Contact</a></li>
+        <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
+<li><Link to="/about" onClick={() => setIsOpen(false)}>About</Link></li>
+<li><Link to="/projects" onClick={() => setIsOpen(false)}>Projects</Link></li>
+<li><Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link></li>
       </ul>
     </nav>
   );
